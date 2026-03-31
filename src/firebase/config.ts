d7750fd1ae102acf1fdd14edcd2551f2a6dbffa6
Felype Naiff <felypenaiff01@gmail.com>
2026-03-31 21:57:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3547326761-4d100",
+  "appId": "1:417952317690:web:2e7d9b20b83a7fdaa74943",
+  "apiKey": "AIzaSyBf4Y-YgISvrOmRCf9bt3aTlXiMUhP-_Go",
+  "authDomain": "studio-3547326761-4d100.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "417952317690"
+};
